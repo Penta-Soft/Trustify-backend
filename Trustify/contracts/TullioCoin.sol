826d18eb1536b04d3a9f227b473c36c6fca6e93e
lazza <nicola.lazzarin89@gmail.com>
2023-03-13 19:15:50 +0100

    tulliocoin mi piace di più

--- Trustify/contracts/TullioCoin.sol
+++ Trustify/contracts/TullioCoin.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TullioCoin is ERC20 {
-    constructor() ERC20("TullioCoin", "TC") {}
+    constructor() ERC20("TullioCoin", "TullioCoin") {}
 
     function drip() public {
-        _mint(msg.sender, 100000 ether);//1 ether corrisponde a moltiplicare per 10^18
+        _mint(msg.sender, 100000 ether); //1 ether corrisponde a moltiplicare per 10^18
     }
 }
