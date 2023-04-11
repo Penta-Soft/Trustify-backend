7eec3a0a586babbd1c0e48cea1160eb6d3469fb7
nicola <nicola.lazzarin89@gmail.com>
2023-04-11 19:41:29 +0200

    added deleteReview test

--- Trustify/test/Trustify.unit.test.js
+++ Trustify/test/Trustify.unit.test.js
@@ -171,11 +171,11 @@ contract('Trustify-unit-test', function ([ customerAddress, customerAddress2, cu
         const {0: review, 1: stars, 2: addresses} = result;
 
         expect(review[0]).to.equal("HELOOOOOOO");
-        expect(review[1]).to.equal("HELOOOOOOOO");
+        expect(review[1]).to.equal("HELOOOOOO");
         expect(stars[0].toString()).to.equal("2");
-        expect(stars[1].toString()).to.equal("1");
+        expect(stars[1].toString()).to.equal("3");
         expect(addresses[0]).to.equal(ecommerceAddress2);
-        expect(addresses[1]).to.equal(ecommerceAddress3);
+        expect(addresses[1]).to.equal(ecommerceAddress);
     });
 
     it('Get a non existing review from GetNMyReview', async function () {
@@ -239,15 +239,15 @@ contract('Trustify-unit-test', function ([ customerAddress, customerAddress2, cu
         const result = await holder.GetNMyReview(0, 25,{from: customerAddress});
         const {0: review, 1: stars, 2: addresses} = result;
 
-        expect(review[0]).to.equal("HELOOOOOO");
+        expect(review[2]).to.equal("HELOOOOOO");
         expect(review[1]).to.equal("HELOOOOOOO");
-        expect(review[2]).to.equal("HELOOOOOOOO");
-        expect(stars[0].toString()).to.equal("3");
+        expect(review[0]).to.equal("HELOOOOOOOO");
+        expect(stars[2].toString()).to.equal("3");
         expect(stars[1].toString()).to.equal("2");
-        expect(stars[2].toString()).to.equal("1");
-        expect(addresses[0]).to.equal(ecommerceAddress);
+        expect(stars[0].toString()).to.equal("1");
+        expect(addresses[2]).to.equal(ecommerceAddress);
         expect(addresses[1]).to.equal(ecommerceAddress2);
-        expect(addresses[2]).to.equal(ecommerceAddress3);
+        expect(addresses[0]).to.equal(ecommerceAddress3);
 
     });
 
@@ -298,10 +298,10 @@ contract('Trustify-unit-test', function ([ customerAddress, customerAddress2, cu
 
         const result = await holder.GetNCompanyReview(1, 2, ecommerceAddress);
         const {0: review, 1: stars} = result;
-        expect(review[0]).to.equal("HELOOOOOOO");
-        expect(review[1]).to.equal("HELOOOOOOOO");
-        expect(stars[0].toString()).to.equal("2");
-        expect(stars[1].toString()).to.equal("1");
+        expect(review[1]).to.equal("HELOOOOOOO");
+        expect(review[0]).to.equal("HELOOOOOOOO");
+        expect(stars[1].toString()).to.equal("2");
+        expect(stars[0].toString()).to.equal("1");
     }); 
 
     it('Get a non existing review from GetNCompanyReview', async function () {
@@ -367,12 +367,12 @@ contract('Trustify-unit-test', function ([ customerAddress, customerAddress2, cu
         const result = await holder.GetNCompanyReview(0, 25, ecommerceAddress, {from: customerAddress});
         const {0: review, 1: stars} = result;
 
-        expect(review[0]).to.equal("HELOOOOOO");
+        expect(review[2]).to.equal("HELOOOOOO");
         expect(review[1]).to.equal("HELOOOOOOO");
-        expect(review[2]).to.equal("HELOOOOOOOO");
-        expect(stars[0].toString()).to.equal("3");
+        expect(review[0]).to.equal("HELOOOOOOOO");
+        expect(stars[2].toString()).to.equal("3");
         expect(stars[1].toString()).to.equal("2");
-        expect(stars[2].toString()).to.equal("1");
+        expect(stars[0].toString()).to.equal("1");
 
     });
 
@@ -399,6 +399,88 @@ contract('Trustify-unit-test', function ([ customerAddress, customerAddress2, cu
         }
 
     });
+
+    it('Write 6 valid review and delete it one', async function () {
+        await coin.drip({from: customerAddress});
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress});
+        await holder.DepositTokens(ecommerceAddress, ethers.parseEther("100"), {from: customerAddress});
+        await holder.WriteAReview(ecommerceAddress, "HELOOOOOO", 1, {from: customerAddress});
+        
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress});
+        await holder.DepositTokens(ecommerceAddress2, ethers.parseEther("100"), {from: customerAddress});
+        await holder.WriteAReview(ecommerceAddress2, "HELOOOOOOO", 2, {from: customerAddress});
+
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress});
+        await holder.DepositTokens(ecommerceAddress3, ethers.parseEther("100"), {from: customerAddress});
+        await holder.WriteAReview(ecommerceAddress3, "HELOOOOOOOO", 3, {from: customerAddress});
+
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress});
+        await holder.DepositTokens(ecommerceAddress4, ethers.parseEther("100"), {from: customerAddress});
+        await holder.WriteAReview(ecommerceAddress4, "HELOOOOOOOOO", 4, {from: customerAddress});
+
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress});
+        await holder.DepositTokens(ecommerceAddress5, ethers.parseEther("100"), {from: customerAddress});
+        await holder.WriteAReview(ecommerceAddress5, "HELOOOOOOOOOO", 5, {from: customerAddress});
+
+        await holder.DeleteReview(ecommerceAddress5, {from: customerAddress});
+
+
+        const result = await holder.GetNMyReview(0, 10,{from: customerAddress});
+        const {0: review, 1: stars, 2: addresses} = result;
+
+        expect(review[3]).to.equal("HELOOOOOO");
+        expect(review[2]).to.equal("HELOOOOOOO");
+        expect(review[1]).to.equal("HELOOOOOOOO");
+        expect(review[0]).to.equal("HELOOOOOOOOO");
+
+        expect(stars[3].toString()).to.equal("1");
+        expect(stars[2].toString()).to.equal("2");
+        expect(stars[1].toString()).to.equal("3");
+        expect(stars[0].toString()).to.equal("4");
+        expect(addresses[3]).to.equal(ecommerceAddress);
+        expect(addresses[2]).to.equal(ecommerceAddress2);
+        expect(addresses[1]).to.equal(ecommerceAddress3);
+        expect(addresses[0]).to.equal(ecommerceAddress4);
+
+    });
+
+    
+    it('Write 6 valid review to an address and delete all review', async function (){
+        await coin.drip({from: customerAddress});
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress});
+        await holder.DepositTokens(ecommerceAddress, ethers.parseEther("100"), {from: customerAddress});
+        await holder.WriteAReview(ecommerceAddress, "HELOOOOOO", 1, {from: customerAddress});
+
+        await coin.drip({from: customerAddress2});
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress2});
+        await holder.DepositTokens(ecommerceAddress, ethers.parseEther("100"), {from: customerAddress2});
+        await holder.WriteAReview(ecommerceAddress, "HELOOOOOOO", 2, {from: customerAddress2});
+
+        await coin.drip({from: customerAddress3});
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress3});
+        await holder.DepositTokens(ecommerceAddress, ethers.parseEther("100"), {from: customerAddress3});
+        await holder.WriteAReview(ecommerceAddress, "HELOOOOOOOO", 3, {from: customerAddress3});
+
+        await coin.drip({from: customerAddress4});
+        await coin.approve(holder.address, ethers.parseEther("100"), {from: customerAddress4});
+        await holder.DepositTokens(ecommerceAddress, ethers.parseEther("100"), {from: customerAddress4});
+        await holder.WriteAReview(ecommerceAddress, "HELOOOOOOOOO", 4, {from: customerAddress4});
+
+        await holder.DeleteReview(ecommerceAddress, {from: customerAddress});
+        await holder.DeleteReview(ecommerceAddress, {from: customerAddress2});
+        await holder.DeleteReview(ecommerceAddress, {from: customerAddress3});
+        await holder.DeleteReview(ecommerceAddress, {from: customerAddress4});
+
+        try {
+            await holder.GetNCompanyReview(0, 10, ecommerceAddress, {from: customerAddress});
+        } catch (error) {
+            expect(error.message).to.equal("Returned error: VM Exception while processing transaction: revert This company have not received any reviews");
+        }
+
+
+    });
+
+
 
 
 });
